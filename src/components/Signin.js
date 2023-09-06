ea7f805882d707b52b212200e422b4f3e7529915
Ashwani Yadav <49113451+Ashwani-Ydv@users.noreply.github.com>
2023-09-06 14:48:51 +0530

    routing fix

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,22 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Signin = () => {
-  console.log("signin");
   return (
     <div>
       <h1>Login</h1>
       <h5>Enter Your Account Login Details.</h5>
       <div>
         <div>
-          <input placeholder="Enter Username or Email" />
+          <input placeholder="Enter Username or Email" type="email" />
         </div>
         <div>
-          <input placeholder="Password" />
+          <input placeholder="Password" type="password" />
         </div>
         <div>
-          <button>Signin</button>
+          <Link to="/dashboard">
+            <button>Signin</button>
+          </Link>
         </div>
-        <div>Don't you have an account? Sign up</div>
+        <div>
+          Don't you have an account?
+          <Link to="/signup">Sign up</Link>
+        </div>
       </div>
     </div>
   );
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,8 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const countries = [
+  { id: "101", name: "Country 1" },
+  { id: "102", name: "Country 2" },
+  { id: "103", name: "Country 3" },
+];
 
 const Signup = () => {
-  console.log("signup");
-  return <div>Signup</div>;
+  const [countryRowId, setCountryRowId] = useState("");
+  return (
+    <div>
+      <h1>Register</h1>
+      <h5>Create Your Company Accounts.</h5>
+      <div>
+        <div>
+          <input placeholder="Full name *" type="text" />
+        </div>
+        <div>
+          <input placeholder="User name *" type="text" />
+        </div>
+        <div>
+          <select
+            value={countryRowId}
+            onChange={(e) => setCountryRowId(e.target.value)}
+          >
+            <option>Select Country *</option>
+            {countries.map((country) => (
+              <option key={country.id} value={country.id}>
+                {country.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <input placeholder="Mobile Number *" type="number" />
+        </div>
+        <div>
+          <input placeholder="Email Id *" type="email" />
+        </div>
+        <div>
+          <input placeholder="Password *" type="Password" />
+        </div>
+        <div>
+          <input placeholder="Refferal ID " type="text" />
+        </div>
+        <div>
+          <button>Register</button>
+        </div>
+        <div>
+          Have an account?
+          <Link to="/">Sign In</Link> here
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
